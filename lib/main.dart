7308ceba5a477f80362eef2f43c2a67b0a14a19f
nio74 <mpaolo74@gmail.com>
2019-12-24 12:14:42 +0100

    work 2412

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,27 @@
 import 'package:flutter/material.dart';
 
-void main() {
+void main() => runApp(MyApp());
   
+
+class MyApp extends StatelessWidget{
+
+final String myText = "Ciao World";
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+      color: Colors.orange[800],
+      padding: const EdgeInsets.all(36),
+      child:
+       Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+        Text(this.myText,textDirection: TextDirection.ltr,),
+        Text(this.myText,textDirection: TextDirection.ltr,)
+
+      ],) 
+  );
+  }
+
 }
