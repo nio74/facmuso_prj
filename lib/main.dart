33291dad413e19cbcdabef7d588ef0a024f97c71
nio74 <mpaolo74@gmail.com>
2019-12-25 10:30:11 +0100

    casaNatale

--- lib/main.dart
+++ lib/main.dart
@@ -6,50 +6,119 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: "MaterialApp",
+        title: "personal budget",
         theme: ThemeData(primarySwatch: Colors.blue),
-        home: LoginPage());
+        home: PbHomePage(),
+        );
   }
 }
 
-class LoginPage extends StatelessWidget {
+class PbHomePage extends StatefulWidget {
+  PbHomePage({Key key}) : super(key: key);
+
+  final String title = "Personal Budget";
+
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Posizionamento')),
-      body: Container(
-          margin: EdgeInsets.only(bottom: 16.0),
-          padding: const EdgeInsets.all(16),
-          child: Column(children: <Widget>[
-            Container(
-                margin: EdgeInsets.only(bottom: 16.0), child: Text('LOGIN')),
-            Container(
-              margin: EdgeInsets.only(bottom: 16.0),
-              child: Row(children: <Widget>[
-                Container(
-                    margin: EdgeInsets.only(right: 16.0),
-                    child: Text('USERNAME')),
-                    Flexible(child: TextField())
-              ]),
-            ),
-            Container(
-              margin: EdgeInsets.only(bottom: 16.0),
-              child: Row(children: <Widget>[
-                Container(
-                    margin: EdgeInsets.only(right: 16.0),
-                    child: Text('PASSWORD')),
-                    Flexible(child: TextField(obscureText: true,))
-              ]),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end ,
-              children: <Widget>[RaisedButton(
-                child: Text('Login'),
-                onPressed: () =>{},
-              )],
-            )
-          ])),
+  _PbHomePageState createState() => _PbHomePageState();
+      
+    }
+  
+  
 
+class _PbHomePageState  extends State<PbHomePage>{
+  int _selectedIndex=0;
+  @override
+  Widget build (BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        actions: <Widget>[
+          IconButton(
+            icon:Icon(Icons.settings),
+            onPressed: () => {})
+            ,
+            IconButton(
+            icon:Icon(Icons.refresh),
+            onPressed: () => {},
+          ),
+        ],
+        ),/*
+      body: Container(),
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+          color: Colors.yellowAccent,
+          height: 50,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              FlatButton(
+                child: Text("Elenco"),
+                onPressed: () => {},
+              ),
+              FlatButton(
+                child: Text("Dettaglio"),
+                onPressed: () => {},
+              ),
+              FlatButton(
+                child: Text("Totali"),
+                onPressed: () => {},
+              )
+            ],
+          ),
+        ),
+      ) ,*/
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.shifting,
+        selectedItemColor:  Colors.black,
+        unselectedItemColor: Colors.greenAccent,
+        currentIndex: _selectedIndex,
+        onTap: (_index){
+          setState(() {
+            _selectedIndex = _index;
+          });
+          
+        },
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            title: Text('Elenco'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            title: Text('Dettaaglio'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today),
+            title: Text('Oggi'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.apps),
+            title: Text('Altro'),
+          ),BottomNavigationBarItem(
+            icon: Icon(Icons.apps),
+            title: Text('Altro2'),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        tooltip: 'Aggingi spesa',
+        onPressed: ()=>{},
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      drawer: Drawer(
+        child: Container(
+          color:Colors.yellow,
+          padding: EdgeInsets.fromLTRB(16, 32, 16, 16),
+          child: Text('Demo Menu'),
+        ),
+      ),
     );
-  }
+ }
+    
 }
+ 
